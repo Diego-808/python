0c5806b3dc82e380f89f0fd43dd1e08edfa98ae7
Diego-808 <diegobonito246@gmail.com>
2024-10-23 17:00:31 -0300

    Add files via upload

---
+++ 01-aula.py
@@ -0,0 +1,6 @@
+# aqui eu posso adicionar comentários
+# continuar na linha de baixo
+print("Diego") # comentários a linha do código
+print('Diego')
+print('Diego Nonato')
+
---
+++ 02-aula.py
@@ -0,0 +1,7 @@
+# atribuição de variáveis
+nome = "Diego"
+idade = 16
+altura = 1.81xibir mensagem na tela
+print(" nome: ", nome, "idade: ",idade,"altura: ",altura,)
+# formatação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
